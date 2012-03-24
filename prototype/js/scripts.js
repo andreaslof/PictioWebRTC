2ae7bf61ec393fc574962dc3d173df8b04561a79
Jonny Strömberg <jonny.stromberg@gmail.com>
2012-03-24 19:33:32 +0100

    Prototype is ready

--- prototype/js/scripts.js
+++ prototype/js/scripts.js
@@ -19,13 +19,28 @@ var Welcome = function() {
 
 var Game = function(player1Name, player2Name) {
     var player1Name = player1Name,
-        player2Name = player2Name;
+        player2Name = player2Name,
+        img = {};
+
+    var player1 = document.getElementById('player1'), // a video element
+        player2 = document.getElementById('player2');
+
+    img.one = "",
+    img.two = "",
+    img.three = "",
+    img.four = "";
+
+    var canvas = document.getElementById('photo');
+    var ctx = canvas.getContext('2d');
+
+
     var init = {
         start: function() {
             $('#game').fadeIn(function() {
                 init.webcam();
                 init.callbacks();
             });
+            $('#action').text("Click go to go!");
             $('#player1-name').text(player1Name);
             $('#player2-name').text(player2Name);
         },
@@ -34,9 +49,7 @@ var Game = function(player1Name, player2Name) {
             isWebkit = false;
 
             function onSuccess(stream) {
-                var player1 = document.getElementById('player1'), // a video element
-                    player2 = document.getElementById('player2'),
-                    source;
+                var source;
 
                 if (!isWebkit) {
                     source = stream;
@@ -46,6 +59,9 @@ var Game = function(player1Name, player2Name) {
 
                 player1.src = source;
                 player2.src = source;
+
+                canvas.width = 640;
+                canvas.height = 480;
             }
 
             function onError() {}
@@ -65,80 +81,102 @@ var Game = function(player1Name, player2Name) {
             $('#go-btn').click(go.firstStep.one);
         }
     };
-
+var timeoutTime = 1000;
     var go = {
         firstStep: {
             one: function() {
-                console.log('height');
+                $('#action').text('Strike a pose!');
                 $('.step').removeClass('highlight');
                 $('#one').addClass('highlight');
-                setTimeout(go.firstStep.two, 1000);
+                setTimeout(go.firstStep.two, timeoutTime);
             },
             two: function() {
                 $('.step').removeClass('highlight');
                 $('#two').addClass('highlight');
-                setTimeout(go.firstStep.three, 1000);
+                setTimeout(go.firstStep.three, timeoutTime);
             },
             three: function() {
                 $('.step').removeClass('highlight');
                 $('#three').addClass('highlight');
-                setTimeout(go.firstStep.go, 1000);
+                setTimeout(go.firstStep.go, timeoutTime);
             },
             go: function() {
+                $('#action').text('Copy!');
                 $('.step').removeClass('highlight');
                 $('#go').addClass('highlight');
-                setTimeout(go.secondStep.one, 3000);
+
+                ctx.drawImage(player1, 0, 0);
+                img.one = canvas.toDataURL('image/webp');
+
+                setTimeout(function() {
+                    ctx.drawImage(player1, 0, 0);
+                    img.three = canvas.toDataURL('image/webp');
+                }, 2000);
+
+                setTimeout(go.secondStep.one, timeoutTime*2);
             }
         },
         secondStep: {
             one: function() {
                 $('.step').removeClass('highlight');
                 $('#one').addClass('highlight');
-                setTimeout(go.secondStep.two, 1000);
+                setTimeout(go.secondStep.two, timeoutTime);
             },
             two: function() {
                 $('.step').removeClass('highlight');
                 $('#two').addClass('highlight');
-                setTimeout(go.secondStep.three, 1000);
+                setTimeout(go.secondStep.three, timeoutTime);
             },
             three: function() {
                 $('.step').removeClass('highlight');
                 $('#three').addClass('highlight');
-                setTimeout(go.secondStep.go, 1000);
+                setTimeout(go.secondStep.go, timeoutTime);
             },
             go: function() {
                 $('.step').removeClass('highlight');
                 $('#go').addClass('highlight');
+                var winner = (Math.random() > 0.3) ? (Math.random() > 0.5) ? player1Name : player2Name : "Draw";
 
                 var img1 = $('<img src="img/player.png" class="player-thumb">');
                 var img2 = $('<img src="img/player.png" class="player-thumb">');
                 var img3 = $('<img src="img/player.png" class="player-thumb">');
                 var img4 = $('<img src="img/player.png" class="player-thumb">');
-                action.archive(img1[0], img2[0], img3[0], img4[0]);
+
+
+                ctx.drawImage(player1, 0, 0);
+                img.two = canvas.toDataURL('image/webp');
+
+                setTimeout(function() {
+                    ctx.drawImage(player1, 0, 0);
+                    img.four = canvas.toDataURL('image/webp');
+
+                    action.archive(img.one, img.two, img.three, img.four, winner);
+                    $('#action').text("Click go to go again!");
+                }, 2000);
             }
         }
-
     };
+/*
+    var capture = function() {
+        ctx.drawImage(video, 0, 0);
+        var img = document.createElement('img');
+        img.src = canvas.toDataURL('image/webp');
+    }
+    */
 
     var action = {
-        archive: function(img1, img2, img3, img4) {
-            var results1 = $('#player1-results'),
-                results2 = $('#player2-results'),
-                item1Tmpl = $('#archive-item').html(),
-                item2Tmpl = $('#archive-item').html(),
-                item1Content = _.template(item1Tmpl, { player1: img1.src, player2: img2.src }),
-                item2Content = _.template(item2Tmpl, { player1: img3.src, player2: img4.src }),
-                item1 = $('<li class="clearfix">'),
-                item2 = $('<li class="clearfix">');
+        archive: function(img1, img2, img3, img4, winner) {
+            console.log(img1);
+            var results = $('#results'),
+                itemTmpl = $('#archive-item').html(),
+                itemContent = _.template(itemTmpl, { player1: img1, player2: img2, player3: img3, player4: img4, winner: winner }),
+                item = $('<div class="clearfix">');
 
-            item1.append(item1Content).hide();
-            item2.append(item2Content).hide();
+            item.append(itemContent).hide();
 
-            results1.prepend(item1);
-            results2.prepend(item2);
+            results.prepend(item);
 
-            item1.slideDown();
-            item2.slideDown();
+            item.slideDown();
         }
     };
 
